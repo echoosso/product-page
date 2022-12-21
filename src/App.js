4a5cabe8837f745b0aabb6521b4cdccc9c313710
echosso-10 <magoleya@gmail.com>
2022-12-21 15:11:28 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import BlogItem from "./BlogCard";
+import { IsArrayEmpty as checkArrayIsEmpty } from "./Utils";
+import "./App.css";
 
-function App() {
+
+class App extends Component {
+  state= {
+    showBlogs : true
+
+  
+  }
+   blogArr = [{
+    id: 1 ,
+    title: " Blog Title 1 ",
+    description: "lorem Ipsum Dolor lorem Ipsum Dolor lorem Ipsum Dolor"
+  },
+  {
+    id: 2 ,
+    title: " Blog Title 2 ",
+    description: "lorem Ipsum Dolor lorem Ipsum Dolor lorem Ipsum Dolor"
+  },
+  {
+    id: 3 ,
+    title: " Blog Title 3 ",
+  description: "lorem Ipsum Dolor lorem Ipsum Dolor lorem Ipsum Dolor"
+}
+]
+ blogCards = checkArrayIsEmpty(this.blogArr) ? []: this.blogArr.map((item,pos) => {
+  console.log(item);
+  return(
+    < BlogItem  className={'Blog'} key={pos} title={item.title} description={item.description} 
+    likeCount ={item.likeCount} id={item.id} onLikeBtnClick ={item.onLikeBtnClick} position={pos} />
+    // <div className="BlogCard" key={item.id}>
+    //     <h3>{item.title} </h3>
+    //     <p>{item.description}</p>
+    //   </div>
+  )
+})
+  onHideBtnClick =() =>{
+    // let updatedState = !this.state.showBlogs;
+  this.setState((prevState,prevProps) => { 
+    return{showBlogs: !prevState.showBlogs}
+  });
+  console.log(this.state.showBlogs)
+ }
+ render(){
+  console.log("Render Called");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button  onClick={this.onHideBtnClick}> {this.state.showBlogs ?' Hide List': 'Show List '}</button>
+      <br></br>
+      {this.state.showBlogs ? this.blogCards : null }
+     { /* <div className="BlogCard">
+        <h3>{blogObj.title} </h3>
+        <p>{blogObj.description}</p>
+      </div>
+      <hr></hr>
+      <div  className="BlogCard">
+        <h3>{blogObj.title} </h3>
+        <p>{blogObj.description}</p>
+      </div>
+      <hr></hr>
+      <div  className="BlogCard">
+        <h3>{blogObj.title} </h3>
+        <p>{blogObj.description}</p>
+  </div> */}
     </div>
   );
+ }
+  
 }
 
 export default App;
